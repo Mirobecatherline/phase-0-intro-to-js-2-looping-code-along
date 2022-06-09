9034a998b9013ce1f650f1d82d38838043e5c6c9
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-06-09 20:25:20 +0300

    commit tree ad299288bf24067f414cd487e3cd0b6a357007cc

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+let names=["Guadalupe", "Ollie", "Aki"];
+function writeCards(names, event="surprise") {
+    const letter=[];
+    for (let index = 0; index < names.length; index++) {
+        
+        letter.push(`Thank you, ${names[index]}, for the wonderful ${event} gift!`);
+    }
+    return letter;
+}
+
+function countDown() {
+    let count=0;
+    while (count<11) {
+        console.log(count++);
+        
+    }
+}
